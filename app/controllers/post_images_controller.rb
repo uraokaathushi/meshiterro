4020f6c20ba6861519b8222166033d3d834fe327
uraokaathushi <149951267+uraokaathushi@users.noreply.github.com>
2023-12-18 13:54:04 +0000

    [finish] 4-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,21 +6,23 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
+    if @post_image.save
     redirect_to post_images_path
+    else
+     render :new
+    end
   end
 
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
     redirect_to post_images_path
-
+    
   end
-
-
-
+  
+ 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resource :favorite, only: [:create, :destroy]
    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-
 end
 
 
